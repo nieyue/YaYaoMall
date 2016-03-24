b9c92551fa413951398836c23ef1b46f680adfae
nieyue <278076304@qq.com>
2016-03-24 19:22:32 +0800

    2016 3 24

--- YaYaoMall/WebRoot/resources/js/base.js
+++ YaYaoMall/WebRoot/resources/js/base.js
@@ -53,15 +53,48 @@ var myTouchEvents={
 };
 
 /**
+ * 模拟登录
+ * 
+ */
+var userData={
+		userInit:{
+		userName:'nieyue',
+		userPassword:hex_sha1('123456'),
+		userIMG:'resources/img/preLoding.jpg',
+		userNiceName:'添加昵称',
+		userSignature:'把你爱好留在这里！',
+		userEmail:'邮箱认证后可以用它登陆',
+		userPhone:'手机号认证后可以用它登陆',
+		userIdentity:'点击认证',
+		userReceiptAddress:'添加收货地址',
+		init:function(){
+			localStorage.getItem("userName", userName);
+		}
+			
+},
+  userPerson:{
+	  userName:'nieyue',
+	  userNiceName:'聂跃',
+		userPassword:hex_sha1('123456'),
+		userIMG:'http://img.mukewang.com/user/54859e4f00019f2a01000100-40-40.jpg',
+		userSignature:'我是一直笑笑笑!',
+		userEmail:'278076545@qq.com',
+		userPhone:'15488654845',
+		userIdentity:'430504194705050468',
+		userReceiptAddress:'湖南长沙岳麓区晟通城',
+		init:function(){
+			localStorage.getItem("userName", userName);
+		}
+}
+
+};
+
+/**
  * 初始化
  * 
  */
 
 myTouchEvents.initTouchEvents();
-
-
-
-
 
 
 
